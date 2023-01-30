c9f22680bc7029eb57ba9f7b39f0d83a89933509
arra1187 <arra1187@pacificu.edu>
2023-01-30 07:41:46 -0800

    Updated fragments

--- app/src/main/java/com/example/costcalculator30/SecondFragment.java
+++ app/src/main/java/com/example/costcalculator30/SecondFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -15,27 +17,41 @@ public class SecondFragment extends Fragment {
 
     private FragmentSecondBinding binding;
 
-    @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
+    private EditText mMoneyCount;
+    private EditText mCurrentRound;
+    private Spinner mTargetTower;
+    private Spinner mTopPath;
+    private Spinner mMiddlePath;
+    private Spinner mBottomPath;
 
-        binding = FragmentSecondBinding.inflate(inflater, container, false);
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState)
+    {
+        binding = FragmentSecondBinding.inflate(inflater, container,
+          false);
         return binding.getRoot();
 
     }
 
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState)
+    {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
+        mMoneyCount = view.findViewById(R.id.enter_current_round);
+        mCurrentRound = view.findViewById(R.id.enter_money_count);
+        mTargetTower = view.findViewById(R.id.target_tower_dropdown);
+        mTopPath = view.findViewById(R.id.target_tower_top_path);
+        mMiddlePath = view.findViewById(R.id.target_tower_middle_path);
+        mBottomPath = view.findViewById(R.id.target_tower_bottom_path);
+
+        /*binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(SecondFragment.this)
                         .navigate(R.id.action_SecondFragment_to_FirstFragment);
             }
-        });
+        });*/
     }
 
     @Override
